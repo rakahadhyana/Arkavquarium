0994c1d58d7f9376ba02e6051da58e4055845775
Nira R Ramadhani <ramadhani.nira@gmail.com>
2018-04-25 03:37:45 +0700

    Add files via upload

--- src/Food.java
+++ src/Food.java
@@ -1,34 +1,43 @@
 /**
- * This class implements Food and inherits Entity class and Interface
+ * This class implements Food and inherits Entity class and Interface.
  * @author Funtastic4
  * @version 1.0
  */
 
 class Food extends Entity {
-    /**
-     * This constructs a food with a specified (x,y) position
-     * @param x
-     * @param y
-     */
-	public Food(double x, double y) {
-        super(x, y);
-        this.x = x;
-        this.y = y;
+  /**
+  * This constructs a food with a specified (x,y) position.
+  * @param x the x position of entity object
+  * @param y the y position of entity object
+  */
+  public Food(double x, double y) {
+    super(x, y);
+    this.x = x;
+    this.y = y;
+  }
+  /**
+   * This method is used to move the position of Food to
+   * a greater y as long as the value of y is not at 4%
+   * of the bottom of the aquarium.
+   */
+  
+  /**
+   * Override move method from Entity class.
+   * It implements the food's move
+   */
+  @Override
+  public void move() {
+    y++;
+  }
+  
+  /**
+  * This method implements food's activity.
+  * @param list of Food
+  */
+  public void lifeCycle(LinkedList<Food> listFood) {
+    move();
+    if (y > 95) {
+      listFood.remove(this);
     }
-	/**
-	 * This method is used to move the position of Food to
-	 * a greater y as long as the value of y is not at 4%
-	 * of the bottom of the aquarium
-	 */
-	@Override
-    public void move() {
-            y++;
-    }
-
-    public void lifeCycle(LinkedList<Food> listFood) {
-        move();
-        if (y > 95) {
-            listFood.remove(this);
-        }
-    }
+  }
 }
