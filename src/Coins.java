f70d052a798be6571f1b73acb2cb681070b14e6d
Muhammad Farhan <mfarhan0304@gmail.com>
2018-04-25 22:12:24 +0700

    Fix w/ JavaDoc and CheckStyle

--- src/Coins.java
+++ src/Coins.java
@@ -1,27 +1,30 @@
-// TODO: Auto-generated Javadoc
 /**
  * This class implements coins and inherit Entity class.
  * @author Funtastic4
  * @version 1.0
  */
 public class Coins extends Entity {
-  
-  /** The money. */
   public static Integer money;
-  
-  /** The value. */
   private int value;
-  
+
   /**
-   * This constructs a coin with a specified (x,y) position.
-   * and value 
-   * @param x coin's axis
-   * @param y coin's ordinate
-   * @param value coin's value
+   * This constructs a coin with a specified (x,y) position
+   * and value.
+   * @param abscissa : x-axis
+   * @param ordinate : y-axis
+   * @param value : coin value
    */
-  public Coins(double x, double y, int value) {
-    super(x, y);
+  public Coins(double abscissa, double ordinate, int value) {
+    super(abscissa, ordinate);
     setValue(value);
+  }
+
+  /**
+   * This is used to set the value of the coins.
+   * @param value : coin value
+   */
+  public void setValue(int value) {
+    this.value = value;
   }
 
   /**
@@ -30,31 +33,21 @@ public class Coins extends Entity {
    * the aquarium.
    */
   public void move() {
-    if (y <= 90) {
-      y += 2;
+    if (ordinate <= 90) {
+      ordinate += 2;
     }
   }
-  
+
   /**
-   * Sets the value.
-   *
-   * @param value the new value
-   */
-  public void setValue(int value) {
-    this.value = value;
-  }
-  
-  /**
-   * Gets the value.
-   *
-   * @return the value
+   * This returns the current value of the coin.
+   * @return this coin's value
    */
   public int getValue() {
     return this.value;
   }
-    
+
   /**
-   * This method do coins activity.
+   * Coins move down to the bottom of Aquarium.
    */
   public void lifeCycle() {
     move();
--- src/Snail.java
+++ src/Snail.java
@@ -1,74 +1,91 @@
+import static java.lang.Math.pow;
+import static java.lang.Math.round;
+
 /**
  * This class implements the snail and it inherits Entity class.
  * @author Funtastic4
  * @version 1.0
  */
 public class Snail extends Entity {
+  private Direction direction;
+
   /**
-   * This constructs Snail object and initialize it's position (x,y) by calling it's 
-   * parent constructor and set it's direction to right.
+   * Snail default constructor.
    */
   public Snail() {
     super(50, 100);
     setDirection(Direction.RIGHT);
   }
-  
+
   /**
-   * This return the distance from snail to @param c.
-   * @param c the coin that want to look the dista.nce
-   * @return
+   * Function to calculate Snail to one Coins in Aquarium.
+   * @param other : Coin which already in Aquarium
+   * @return the distance between Snail to Coin
    */
-  public double distanceTo(Coins c) {
-    return (Math.pow(Math.pow(c.getX() - x, 2) + Math.pow(c.getY() - y, 2), 0.5));
+  public double distanceTo(Coins other) {
+    return pow((pow(other.getX() - getX(), 2) + (pow(other.getY() - getY(), 2))), 0.5);
   }
 
+  /**
+   * Snail movement procedure.
+   */
   @Override
   public void move() {}
-    
+
   /**
-   * This make the snail move to @param c.
-   * @param c the coin that want to move to
+   * Procedure used for Snail to move closer to Coins.
+   * @param coinsTarget : Chased Coins relative to Snail
    */
-  public void moveToCoins(Coins c) {
-    if (x < Math.round(c.getX())) {
+  public void moveToCoins(Coins coinsTarget) {
+    if (abscissa < round(coinsTarget.getX())) {
       setDirection(Direction.RIGHT);
-      x += getSpeed();
-    } else if (x > Math.round(c.getX())) {
+      abscissa += getSpeed();
+    } else if (abscissa > round(coinsTarget.getX())) {
       setDirection(Direction.LEFT);
-      x -= getSpeed();
+      abscissa -= getSpeed();
     }
   }
 
   /**
-   * This make snail do it's activity.
-   * @param listCoins list coin that affected from snail activity
+   * Procedure that describe how Snail live in Aquarium
+   * Chasing Coins if Coin's range.
+   * @param listCoins : Coins list in Aquarium
    */
   public void lifeCycle(LinkedList<Coins> listCoins) {
     int idx = 0;
+
     if (!listCoins.isEmpty()) {
       Coins closeCoins = listCoins.get(idx);
       idx++;
+
       while (idx < listCoins.getIdx()) {
         if (distanceTo(closeCoins) > distanceTo(listCoins.get(idx))) {
           closeCoins = listCoins.get(idx);
         }
         idx++;
       }
+
       moveToCoins(closeCoins);
-      if (Math.round(x) == Math.round(closeCoins.getX()) && y - 10 <= closeCoins.getY()) {
+      if (round(abscissa) == round(closeCoins.getX()) && ordinate - 10 <= closeCoins.getY()) {
         Coins.money += closeCoins.getValue();
         listCoins.remove(closeCoins);
       }
     }
   }
 
-  public Direction getDirection() {
-    return direction;
-  }
-
+  /**
+   * Direction setter.
+   * @param direction : updated direction.
+   */
   public void setDirection(Direction direction) {
     this.direction = direction;
   }
 
-  private Direction direction;
+  /**
+   * Direction getter.
+   * @return current direction
+   */
+  public Direction getDirection() {
+    return direction;
+  }
 }
