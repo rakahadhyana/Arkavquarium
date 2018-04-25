512005e891f043211713decf6eb32e6ac29839cf
Nira R Ramadhani <ramadhani.nira@gmail.com>
2018-04-25 22:45:11 +0700

    Add files via upload

--- src/Entity.java
+++ src/Entity.java
@@ -6,92 +6,92 @@ import java.util.Comparator;
  * @version 1.0
  */
 public abstract class Entity implements Comparator<Entity>, Interface {
-    /**
-     * This Construct abstract object entity with random position.
-     */
-    public Entity() {
-        this.abscissa = Math.random() * Aquarium.getSize();
-        this.ordinate = Math.random() * Aquarium.getSize();
-        this.speed = 1;
+  /**
+   * This Construct abstract object entity with random position.
+   */
+  public Entity() {
+    this.abscissa = Math.random() * Aquarium.getSize();
+    this.ordinate = Math.random() * Aquarium.getSize();
+    this.speed = 1;
+  }
+
+  /**
+   * This Construct abstract object entity with specified position.
+   * @param abscissa entity's axis
+   * @param ordinate entity's ordinate
+   */
+  public Entity(double abscissa, double ordinate) {
+    this.abscissa = abscissa;
+    this.ordinate = ordinate;
+    this.speed = 1;
+  }
+
+  @Override
+  public abstract void move();
+
+  @Override
+  public int compare(Entity e1, Entity e2) {
+    return 0;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj instanceof Entity) {
+      Entity newE = (Entity)obj;
+      return (abscissa == newE.getX() && ordinate == newE.getY());
     }
+    return false;
+  }
 
-    /**
-     * This Construct abstract object entity with specified position.
-     * @param abscissa entity's axis
-     * @param ordinate entity's ordinate
-     */
-    public Entity(double abscissa, double ordinate) {
-        this.abscissa = abscissa;
-        this.ordinate = ordinate;
-        this.speed = 1;
-    }
+  /**
+   * This sets the Entity's axis.
+   * @param abscissa the entity's axis
+   */
+  public void setX(double abscissa) {
+    this.abscissa = abscissa;
+  }
 
-    @Override
-    public abstract void move();
+  /**
+   * This sets the Entity's ordinate.
+   * @param ordinate the entity's ordinate
+   */
+  public void setY(double ordinate) {
+    this.ordinate = ordinate;
+  }
 
-    @Override
-    public int compare(Entity e1, Entity e2) {
-        return 0;
-    }
+  /**
+  * This sets the Entity's speed.
+  * @param speed the entity's speed
+  */
+  public void setSpeed(double speed) {
+    this.speed = speed;
+  }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (obj instanceof Entity) {
-            Entity newE = (Entity)obj;
-            return (abscissa == newE.getX() && ordinate == newE.getY());
-        }
-        return false;
-    }
+  /**
+   * This returns this' axis.
+   * @return this' axis
+   */
+  public double getX() {
+    return this.abscissa;
+  }
 
-    /**
-     * This sets the Entity's axis.
-     * @param abscissa the entity's axis
-     */
-    public void setX(double abscissa) {
-        this.abscissa = abscissa;
-    }
+  /**
+   * This returns this' ordinate.
+   * @return this' ordinate
+   */
+  public double getY() {
+    return this.ordinate;
+  }
 
-    /**
-     * This sets the Entity's ordinate.
-     * @param ordinate the entity's ordinate
-     */
-    public void setY(double ordinate) {
-        this.ordinate = ordinate;
-    }
+  /**
+  * This returns this' speed.
+  * @return this' speed
+  */
+  public double getSpeed() {
+    return this.speed;
+  }
 
-    /**
-     * This sets the Entity's speed.
-     * @param speed the entity's speed
-     */
-    public void setSpeed(double speed) {
-        this.speed = speed;
-    }
-
-    /**
-     * This returns this' axis.
-     * @return this' axis
-     */
-    public double getX() {
-        return this.abscissa;
-    }
-
-    /**
-     * This returns this' ordinate.
-     * @return this' ordinate
-     */
-    public double getY() {
-        return this.ordinate;
-    }
-
-    /**
-     * This returns this' speed.
-     * @return this' speed
-     */
-    public double getSpeed() {
-        return this.speed;
-    }
-
-    protected double abscissa;
-    protected double ordinate;
-    protected double speed;
+  protected double abscissa;
+  protected double ordinate;
+  protected double speed;
 }
