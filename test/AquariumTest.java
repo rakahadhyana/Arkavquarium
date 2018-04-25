69fccc3bb484d1f1397f30390eafcc441bdfd9d9
Muhammad Farhan <mfarhan0304@gmail.com>
2018-04-25 23:31:10 +0700

    Add files via upload

--- test/AquariumTest.java
+++ test/AquariumTest.java
@@ -3,23 +3,22 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AquariumTest {
-    /**
-     * This returns the current size of the aquarium
-     * @return this aquarium's size
-     */
+
     @Test
     public static void getSize() {
         Aquarium testA = new Aquarium();
         assertEquals(100, testA.getSize());
     }
+
     @Test
     public static void getPercentSize() {
         Aquarium testA = new Aquarium();
-        assertEquals(100, testA.getPercentSize());
+        assertEquals(5, testA.getPercentSize(5), 0);
     }
+
     @Test
     public static void getBorder() {
         Aquarium testA = new Aquarium();
-        assertEquals(100 - 100, testA.getBorder());
+        assertEquals(95, testA.getBorder(5), 0);
     }
 }
